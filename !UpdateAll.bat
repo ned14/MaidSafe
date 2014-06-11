4b3170b982d54ff9e0d0fe5c179533136ed0f6c9
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2014-06-11 18:43:08 +0100

    Duplicated script on Windows instead of trying to be clever.

--- !UpdateAll.bat
+++ !UpdateAll.bat
@@ -1,1 +1,23 @@
-@cmd /c "%~dpn0.sh %*"
+rem @cmd /c "%~dpn0.sh %*"
+
+git fetch origin
+git fetch main
+git checkout next
+git merge remotes/origin/next
+git merge remotes/main/next
+git checkout -f next_ned14
+git merge next
+
+git submodule update --init --recursive
+git submodule foreach git checkout next
+git checkout next
+
+cd src\rudp
+git fetch main
+git merge remotes/main/next
+cd ..\..
+
+cd src\routing
+git fetch main
+git merge remotes/main/next
+cd ..\..
