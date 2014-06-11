316b1a93a8af1d41013872227143d267ee51cf6a
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2014-06-11 18:18:50 +0100

    Added Maidsafe fetch automation scripts.

---
+++ !UpdateAll.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+git fetch origin
+git fetch main
+git checkout next
+git merge remotes/origin/next
+git merge remotes/main/next
+git checkout next_ned14
+git merge next
+
+git submodule update --init --recursive
+git submodule foreach git checkout next
+git checkout next
+
+cd src/rudp
+git fetch main
+git merge remotes/main/next
+cd ../..
+
+cd src/routing
+git fetch main
+git merge remotes/main/next
+cd ../..
+
