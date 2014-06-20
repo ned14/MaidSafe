12c67401c224ee36e3254d3944ac6ec44594f73c
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2014-06-20 17:59:16 +0100

    Updated update script

--- !UpdateAll.bat
+++ !UpdateAll.bat
@@ -1,16 +1,17 @@
 rem @cmd /c "%~dpn0.sh %*"
 
+rem attrib +r !UpdateAll.bat
 git fetch origin
 git fetch main
 git checkout next
 git merge remotes/origin/next
 git merge remotes/main/next
-git checkout -f next_ned14
-git merge next
+rem git checkout -f next_ned14
+rem git merge next
 
 git submodule update --init --recursive
 git submodule foreach git checkout next
-git checkout next
+rem git checkout next
 
 cd src\rudp
 git fetch main
